fde1d782af427b8774698536a20db5bb69512ed1
Dualdragoon <hosleraaron06@gmail.com>
2016-12-29 19:05:44 -0500

    Some more work on the dialogue system.

--- Tower Defense Project/Tower_Defense_Project/Dialogue.cs
+++ Tower Defense Project/Tower_Defense_Project/Dialogue.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml;
+using SharpDX;
+using SharpDX.Toolkit.Graphics;
 
 namespace Tower_Defense_Project
 {
@@ -27,32 +29,114 @@ namespace Tower_Defense_Project
 			Result = result;
 		}
 	}
+
 	private class DialogueNode
 	{
+		public string Name { get; set; }
 		public string Line { get; private set; }
 
 		public DialogueNode Node { get; private set; }
-		public List<DialogueChoice> Choices = new List<DialogueChoice>();
+		public List<DialogueChoice> Choices { get; private set; }
+		public Texture2D Portrait { get; private set; }
 
-		public DialogueNode(string line, DialogueNode node)
+		public DialogueNode(string line, Texture2D portrait, DialogueNode node)
 		{
 			Line = line;
+			Portrait = portrait;
 			Node = node;
+			Name = "???";
 		}
 
-		public DialogueNode(string line, DialogueNode node, params DialogueChoice[] choices)
+		public DialogueNode(string line, Texture2D portrait, DialogueNode node, params DialogueChoice[] choices)
 		{
 			Line = line;
+			Portrait = portrait;
 			Choices = choices.ToList();
+			Name = "???";
 
 			if (choices[0].ChoiceType != "Node")
 			{
 				Node = node;
 			}
 		}
+
+		public void Clear()
+		{
+			if (Choices != null) Choices.Clear();
+			Node.Clear();
+		}
 	}
+
 	class Dialogue
 	{
+		private DialogueNode node;
+		SpriteFont font;
 
+		public DialogueNode Node
+		{
+			get { return node; }
+			set { node = value; }
+		}
+
+		public Dialogue()
+		{
+			font = Main.GameContent.Load<SpriteFont>("Fonts/Font");
+		}
+
+		public void LoadDialogue(string dialogueName)
+		{
+			XmlDocument doc = new XmlDocument();
+			doc.Load(string.Format("{0}.xml", dialogueName));
+			XmlNode root = doc.SelectSingleNode("/Dialogue");
+			
+			foreach (XmlNode i in root)
+			{
+				string name = i.Attributes["name"].InnerText;
+			}
+		}
+
+		private void LoadLine(string line, DialogueNode nod)
+		{
+
+		}
+
+		public string WrapText(string text, float maxLineWidth, float maxLineHeight)
+		{
+			text.Replace("\n", "");
+			text.Replace("\r", "");
+			string[] words = text.Split(' ');
+			string newText = "";
+			float lineWidth = 0;
+			float lineHeight = font.MeasureString("A").Y;
+			float spaceWidth = font.MeasureString(" ").X;
+
+			foreach (string word in words)
+			{
+				Vector2 size = font.MeasureString(word);
+
+				if (lineWidth + size.X < maxLineWidth)
+				{
+					newText += (word + " ");
+					lineWidth += size.X + spaceWidth;
+				}
+				else
+				{
+					if (lineHeight + size.Y < maxLineHeight)
+					{
+						newText += ("\n" + word + " ");
+						lineWidth = size.X + spaceWidth;
+						lineHeight += size.Y; 
+					}
+					else
+					{
+						newText += ("\t" + word + " ");
+						lineWidth = size.X + spaceWidth;
+						lineHeight = size.Y;
+					}
+				}
+			}
+
+			return newText;
+		}
 	}
 }
