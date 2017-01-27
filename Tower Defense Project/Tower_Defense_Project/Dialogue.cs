1c72eac4b4a7447db3393814a7e9eb6b1a10dd8f
dualdragoon <hosleraaron06@gmail.com>
2017-01-27 00:06:15 -0500

    Started work back up on the Dialogue system. *Still needs more work. Not functional.*

--- Tower Defense Project/Tower_Defense_Project/Dialogue.cs
+++ Tower Defense Project/Tower_Defense_Project/Dialogue.cs
@@ -8,135 +8,200 @@ using SharpDX.Toolkit.Graphics;
 
 namespace Tower_Defense_Project
 {
-	class DialogueChoice
-	{
-		public string ChoiceType { get; private set; }
-		public string Name { get; private set; }
-		public string Result { get; private set; }
+    class DialogueChoice
+    {
+        public string ChoiceType { get; }
+        public string Name { get; }
+        public string Result { get; }
 
-		private event EventHandler choiceSelected;
+        private Dialogue ParentDialogue { get; }
 
-		public event EventHandler ChoiceSelected
-		{
-			add { choiceSelected += value; }
-			remove { choiceSelected -= value; }
-		}
+        private event EventHandler choiceSelected;
 
-		public DialogueChoice(string type, string name, string result)
-		{
-			ChoiceType = type;
-			Name = name;
-			Result = result;
-		}
-	}
+        public event EventHandler ChoiceSelected
+        {
+            add { choiceSelected += value; }
+            remove { choiceSelected -= value; }
+        }
 
-	class DialogueNode
-	{
-		public string Name { get; set; }
-		public string Line { get; private set; }
+        public DialogueChoice(Dialogue dialogue, string type, string name, string result)
+        {
+            ParentDialogue = dialogue;
+            ChoiceType = type;
+            Name = name;
+            Result = result;
 
-		public DialogueNode Node { get; private set; }
-		public List<DialogueChoice> Choices { get; private set; }
-		public Texture2D Portrait { get; private set; }
+            if (type == "Node")
+            {
+                ChoiceSelected += (object sender, EventArgs e) =>
+                {
+                    ParentDialogue.LoadNode("Result");
+                };
+            }
+            else if (type == "Console")
+            {
+                ChoiceSelected += (object sender, EventArgs e) =>
+                {
+                    Console.WriteLine(Result);
+                };
+            }
+        }
+    }
 
-		public DialogueNode(string line, Texture2D portrait, DialogueNode node)
-		{
-			Line = line;
-			Portrait = portrait;
-			Node = node;
-			Name = "???";
-		}
+    class DialogueNode
+    {
+        public string Name { get; set; }
+        public string Line { get; }
 
-		public DialogueNode(string line, Texture2D portrait, DialogueNode node, params DialogueChoice[] choices)
-		{
-			Line = line;
-			Portrait = portrait;
-			Choices = choices.ToList();
-			Name = "???";
+        public string DestinationNode { get; }
+        public List<DialogueChoice> Choices { get; }
+        public List<RectangleF> Rectangles { get; }
+        public Texture2D Portrait { get; }
 
-			if (choices[0].ChoiceType != "Node")
-			{
-				Node = node;
-			}
-		}
+        public DialogueNode(string line, Texture2D portrait, string node)
+        {
+            Line = line;
+            Portrait = portrait;
+            DestinationNode = node;
+            Name = "???";
+        }
 
-		public void Clear()
-		{
-			if (Choices != null) Choices.Clear();
-			Node.Clear();
-		}
-	}
+        public DialogueNode(string line, Texture2D portrait, string node, params DialogueChoice[] choices)
+        {
+            Line = line;
+            Portrait = portrait;
+            Choices = choices.ToList();
+            Name = "???";
 
-	class Dialogue
-	{
-		private DialogueNode node;
-		SpriteFont font;
+            if (choices[0].ChoiceType != "Node")
+            {
+                DestinationNode = node;
+            }
 
-		public DialogueNode Node
-		{
-			get { return node; }
-			set { node = value; }
-		}
+            for (int i = Choices.Count; i > 0; i--)
+            {
+                
+            }
+        }
 
-		public Dialogue()
-		{
-			font = Main.GameContent.Load<SpriteFont>("Fonts/Font");
-		}
+        public void Clear()
+        {
+            if (Choices != null) Choices.Clear();
+        }
+    }
 
-		public void LoadDialogue(string dialogueName)
-		{
-			XmlDocument doc = new XmlDocument();
-			doc.Load(string.Format("{0}.xml", dialogueName));
-			XmlNode root = doc.SelectSingleNode("/Dialogue");
-			
-			foreach (XmlNode i in root)
-			{
-				string name = i.Attributes["name"].InnerText;
-			}
-		}
+    class Dialogue
+    {
+        private DialogueNode node;
+        SpriteFont font;
 
-		private void LoadLine(string line, DialogueNode nod)
-		{
+        Dictionary<string, XmlNode> Nodes { get; set; }
 
-		}
+        public DialogueNode Node
+        {
+            get { return node; }
+            set { node = value; }
+        }
 
-		public string WrapText(string text, float maxLineWidth, float maxLineHeight)
-		{
-			text.Replace("\n", "");
-			text.Replace("\r", "");
-			string[] words = text.Split(' ');
-			string newText = "";
-			float lineWidth = 0;
-			float lineHeight = font.MeasureString("A").Y;
-			float spaceWidth = font.MeasureString(" ").X;
+        public Dialogue()
+        {
+            font = Main.GameContent.Load<SpriteFont>("Fonts/Font");
+        }
 
-			foreach (string word in words)
-			{
-				Vector2 size = font.MeasureString(word);
+        public void LoadDialogue(string dialogueName)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(string.Format("{0}.xml", dialogueName));
+            XmlNode root = doc.SelectSingleNode("/Dialogue");
 
-				if (lineWidth + size.X < maxLineWidth)
-				{
-					newText += (word + " ");
-					lineWidth += size.X + spaceWidth;
-				}
-				else
-				{
-					if (lineHeight + size.Y < maxLineHeight)
-					{
-						newText += ("\n" + word + " ");
-						lineWidth = size.X + spaceWidth;
-						lineHeight += size.Y; 
-					}
-					else
-					{
-						newText += ("\t" + word + " ");
-						lineWidth = size.X + spaceWidth;
-						lineHeight = size.Y;
-					}
-				}
-			}
+            foreach (XmlNode i in root)
+            {
+                Nodes.Add(i.Attributes["idNum"].InnerText, i);
+            }
 
-			return newText;
-		}
-	}
+            LoadNode("Start");
+        }
+
+        public void LoadNode(string idNum)
+        {
+            XmlNode root = Nodes[idNum];
+            List<DialogueChoice> dialogueChoices = new List<DialogueChoice>();
+            List<XmlNode> choices = new List<XmlNode>();
+            string line = "";
+
+            foreach (XmlNode i in root)
+            {
+                string type = i.Attributes["type"].InnerText;
+                if (type == "Speech")
+                {
+                    line = i.InnerText;
+                }
+                else if (type == "Choice")
+                {
+                    foreach (XmlNode l in i)
+                    {
+                        choices.Add(l);
+                    }
+                }
+            }
+
+            foreach (XmlNode i in choices)
+            {
+                string type = i.Attributes["type"].InnerText, name = i.Attributes["name"].InnerText, result = i.InnerText;
+                dialogueChoices.Add(new DialogueChoice(this, type, name, result));
+            }
+
+            line = WrapText(line, Main.Scale.X, 150);
+
+            Texture2D portrait = Main.GameContent.Load<Texture2D>(string.Format("Dialogues/{0}/{1}_{2}", "DialogueTitle", root.Attributes["name"].InnerText, root.Attributes["image"].InnerText));
+
+            if (dialogueChoices.Count == 0)
+            {
+                Node = new DialogueNode(line, portrait, root.Attributes["destination"].InnerText);
+            }
+            else if (dialogueChoices[0].ChoiceType == "Node")
+            {
+                Node = new DialogueNode(line, portrait, "", dialogueChoices.ToArray());
+            }
+        }
+
+        public string WrapText(string text, float maxLineWidth, float maxLineHeight)
+        {
+            text.Replace("\n", " ");
+            text.Replace("\r", "");
+            string[] words = text.Split(' ');
+            string newText = "";
+            float lineWidth = 0;
+            float lineHeight = font.MeasureString("A").Y;
+            float spaceWidth = font.MeasureString(" ").X;
+
+            foreach (string word in words)
+            {
+                Vector2 size = font.MeasureString(word);
+
+                if (lineWidth + size.X < maxLineWidth)
+                {
+                    newText += (word + " ");
+                    lineWidth += size.X + spaceWidth;
+                }
+                else
+                {
+                    if (lineHeight + size.Y < maxLineHeight)
+                    {
+                        newText += ("\n" + word + " ");
+                        lineWidth = size.X + spaceWidth;
+                        lineHeight += size.Y;
+                    }
+                    else
+                    {
+                        newText += ("\t" + word + " ");
+                        lineWidth = size.X + spaceWidth;
+                        lineHeight = size.Y;
+                    }
+                }
+            }
+
+            return newText;
+        }
+    }
 }
